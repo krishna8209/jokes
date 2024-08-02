508f63670e69b137e98af1175809022cd246d76e
krishnapunglia <143738310+krishna8209@users.noreply.github.com>
2024-08-02 23:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+const speakBtn = document.getElementById('speakBtn');
+const audio = document.getElementById('audioplayer');
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener('click', generateJoke);
+speakBtn.addEventListener('click', speakJoke);
 
-generateJoke()
+generateJoke();
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    };
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
 
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
+    jokeEl.innerHTML = data.joke;
 }
 
-// USING .then()
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = document.getElementById('audioplayer');
+    var playButton = document.getElementById('jokeBtn');
 
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
-// }
+    playButton.addEventListener('click', function () {
+        audio.play();
+    });
+});
+
+function speakJoke() {
+    const textToRead = jokeEl.innerText;
+
+    if ('speechSynthesis' in window) {
+        const synth = window.speechSynthesis;
+        
+        // Create a new instance of SpeechSynthesisUtterance
+        const utterance = new SpeechSynthesisUtterance(textToRead);
+
+        // Event listener to re-enable the speak button when speech ends
+        utterance.addEventListener('end', function () {
+            speakBtn.disabled = false;
+        });
+
+        // Event listener for handling errors
+        utterance.addEventListener('error', function (event) {
+            console.error('SpeechSynthesisUtterance Error:', event.error);
+        });
+
+        // Start speech synthesis
+        synth.speak(utterance);
+
+        // Disable the speak button while speech is playing
+        speakBtn.disabled = true;
+    } else {
+        alert('Sorry, your browser does not support the Web Speech API.');
+    }
+}
